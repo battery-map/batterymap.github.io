671407fa5f5b9ee04cf5488e960ca1ded0470fc3
Simakov Konstantin <gigabytetheone@gmail.com>
2014-04-12 00:22:11 +0600

    E-mail for contact

--- scripts/contact.php
+++ scripts/contact.php
@@ -1,8 +1,8 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if(!empty($_POST['contactname']) && !empty($_POST['contactemail']) && !empty($_POST['contactmessage'])) {
-	$to = 'email@address.com'; // Replace with your email.
-	$body = "Name: {$_POST['contactname']}\n\nEmail: {$_POST['contactemail']}\n\nWebsite: {$_POST['contactwebsite']}\n\nMessage: {$_POST['contactmessage']}";
+	$to = 'gigabytetheone@gmail.com'; // Replace with your email.
+	$body = "Name: {$_POST['contactname']}\n\nEmail: {$_POST['contactemail']}\n\nMessage: {$_POST['contactmessage']}";
 	mail($to, "Contact Form Submission", $body, "From: {$_POST['contactemail']}");
     }
 }
