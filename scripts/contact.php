3ac22480e8396097ba4d9d4206c369c279c55652
Simakov Konstantin <gigabytetheone@gmail.com>
2014-04-12 14:31:30 +0600

    Contact form

--- scripts/contact.php
+++ scripts/contact.php
@@ -1,9 +1,32 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if(!empty($_POST['contactname']) && !empty($_POST['contactemail']) && !empty($_POST['contactmessage'])) {
-	$to = 'gigabytetheone@gmail.com'; // Replace with your email.
-	$body = "Name: {$_POST['contactname']}\n\nEmail: {$_POST['contactemail']}\n\nMessage: {$_POST['contactmessage']}";
-	mail($to, "Contact Form Submission", $body, "From: {$_POST['contactemail']}");
-    }
+		$to = 'gigabytetheone@gmail.com'; // Replace with your email.
+		$body = "Name: {$_POST['contactname']}\n\nEmail: {$_POST['contactemail']}\n\nMessage: {$_POST['contactmessage']}";
+	
+		$url = 'https://api.sendgrid.com/api/mail.send.json';
+		$user = 'azure_c6c9355158321572406a74a0ccea61c1@azure.com';
+		$pass = 'A1qvnFVxOfdI28u'; 
+		
+		$params = array(
+		  'api_user' => $user,
+		  'api_key' => $pass,
+		  'to' => $to,
+		  'subject' => "Contact Form Submission",
+		  'text' => $body,
+		  'from' => 'info@battery-map.ru',
+		);
+		
+		$options = array(
+		    'http' => array(
+		        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+		        'method'  => 'POST',
+		        'content' => http_build_query($params),
+		    ),
+		);
+		$context  = stream_context_create($options);
+		$result = file_get_contents($url, false, $context);
+		var_dump($result);
+	}
 }
 ?>
