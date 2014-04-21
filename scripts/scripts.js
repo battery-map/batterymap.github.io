eb5ef53d54be91fc2bbcba05f17db2e3d0718d8f
Simakov Konstantin <gigabytetheone@gmail.com>
2014-04-21 16:16:48 +0600

    VK logo

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -160,7 +160,7 @@
 	var host = slashes.concat(window.location.hostname);
 	var title = "Не выбрасывай батарейку! Узнай где можно сдать её в утилизацию";
 	var url = host;
-	var imageUrl = host + "/images/battery_logo_min.png";
+	var imageUrl = host + "/images/battery_logo_vk.png";
 
 	$('#twitter-share-button').click(function(event) {
 		var shareUrl = "https://twitter.com/intent/tweet?url=" + url + "&text=" + title;
